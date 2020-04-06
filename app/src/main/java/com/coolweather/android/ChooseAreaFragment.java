58fbdbb1561d67f2f6009693213cb513fcdd4df2
long49526518 <1223979620@qq.com>
2020-04-06 04:10:09 +0000

    Add Fragment

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -38,6 +38,10 @@ public class ChooseAreaFragment extends Fragment {
     //更新代码上传github
     //更新代码上传github
     //更新代码上传github
+    //更新代码上传github
+    //更新代码上传github
+    //更新代码上传github
+    //更新代码上传github
     /**
      * 宏定义试图的不同级别
      */
--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//更新代码上传github
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
